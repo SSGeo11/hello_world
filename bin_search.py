da2228dafc287af84e47e72630ce42c9357bedfe
SSGeo11 <129564222+SSGeo11@users.noreply.github.com>
2025-09-25 15:11:22 +0900

    fix cowsay, bin search, requirements; add hello_world; delete pycache

--- bin_search.py
+++ bin_search.py
@@ -1,20 +1,17 @@
-def bin_search(n, t):
+def bin_search(nums, target):
     l = -1
-    r = len(n)
-    m = (l+r)//2
-    while(n[m]!=t and l<=r):
-        if(t > n[m]):
+    r = len(nums)
+    while(l<=r):
+        m = (r+l)//2
+        if nums[m] == target:
+            return m
+        elif(target > nums[m]):
             l = m+1
         else:
-            r = m-1
-        m = (l+r)//2
-    if l>r:
-        return -1
-    else:
-        return m
+            r = m-1    
+    return -1
 
-num = [-1, 2, 3, 5, 6, 8, 11]
-target = 5
-
-print(bin_search(num, target))
+#nums = [5]
+#target = -5
+#print(bin_search(nums, target))
     
--- cowsay.py
+++
@@ -1,2 +0,0 @@
-from cowsay import *
-tux("Moo")
